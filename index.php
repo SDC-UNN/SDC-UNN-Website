1eb9537b837c3a079447d33c9763c81d5592f21c
Obodugo Rapheal <kofacts@gmail.com>
2016-10-02 15:21:12 -1100

    Implemented the homepage
    Yet to be completed though.

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SRC Nigeria</title>
+	<meta charset="utf-8" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="./assets/css/material.css">
+	<!-- I Had to add Lato Fonts -->
+	<link rel="stylesheet" type="text/css" href="./assets/latofonts.css">
+	<link rel="stylesheet" type="text/css" href="./assets/style.css">
+	<script type="text/javascript" src="./assets/js/material.js"></script>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	
+	
+</head>
+<body>
+<!--This is for the Navigation -->
+	<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+	    <header class="mdl-layout__header">
+    		<div class="mdl-layout__header-row">
+     			<span class="mdl-layout-title">Acme</span>
+     		<div class="mdl-layout-spacer"></div>
+     		
+     		<nav class="mdl-navigation">
+		        <a class="mdl-navigation__link" href="#">Club</a>
+		        <a class="mdl-navigation__link" href="#">Events</a>
+		        <a class="mdl-navigation__link" href="#">Gallery</a>
+		        <a class="mdl-navigation__link" href="#">Projects</a>
+		        <a class="mdl-navigation__link" href="#">Contact</a>
+		        <a class="mdl-navigation__link" href="#">Blog</a>
+		        
+      		</nav>	
+    		</div>
+  		</header>
+	
+
+
+<!--This is for the image section -->
+	
+	<section id="mainheader" style="background:linear-gradient(
+      rgba(76,76,76,1), 
+      rgba(255, 0, 0, 0.45)
+    ),url('./images/woekdev.jpg');">
+		
+
+			<div>
+				<center>
+					<h1 style="font-family:'LatoWebLight'; text-align:center; padding-top:20px;">Software and Robotics Club NG</h1><br>
+
+					<span style="font-size:25px; font-family:'LatoWebLight'; color:white;">We Learn By Doing -- No Lame!</span><br>
+
+					<button style="margin-top:20px;" class="mdl-button mdl-button--raised mdl-button--colored" style="background: red;"><i class="material-icons"></i> Join The Team!</button>
+				</center>
+			</div>
+			
+		
+	</section>
+
+	</div>	
+		
+</body>
+</html>
