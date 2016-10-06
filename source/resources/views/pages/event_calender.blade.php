ddcd0793b70f024d745978ab8e37113f411fb364
Kofacts <obodugorapheal@gmail.com>
2016-10-06 08:45:43 -1100

    Added MDL Datepicker, for selecting Events Date in the Events Calendar

--- source/resources/views/pages/event_calender.blade.php
+++ source/resources/views/pages/event_calender.blade.php
@@ -3,14 +3,25 @@
  * Project: srcng.com
  * Author:  Chukwuemeka Nwobodo (jcnwobodo@gmail.com)
  * Date:    9/29/2016
- * Time:    2:55 PM
+ * Time:    3:04 PM
  **/
+ 
  ?>
+
  @extends("app")
 
 
  @section
+ 	<div class="mdl-grid">
+ 		<!-- For the main Section -->
+    <section>
+        <div class="container">
 
+            <p><h3 style="font-family: 'LatoWebLight';">Events</h3> <span>From</span></p>
+            
+        </div>
+    </section>
 
+ 	</div>
  @stop
- 
+ <script type="text/javascript" src="mdl-datepicker-master/mdl-datepicker.js"></script> <!-- Added the MDL Date Picker, Get it on Github Here https://github.com/trentsp/mdl-datepicker and it works with Jquery-->
