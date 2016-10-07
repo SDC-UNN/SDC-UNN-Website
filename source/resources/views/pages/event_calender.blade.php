be35c0d775e0c15c7162d8a444bbf6cb2d6639e0
Kofacts <obodugorapheal@gmail.com>
2016-10-06 16:31:51 -1100

    Events Page modified

--- source/resources/views/pages/event_calender.blade.php
+++ source/resources/views/pages/event_calender.blade.php
@@ -14,12 +14,29 @@
 
  @section
  	<div class="mdl-grid">
- 		<!-- For the main Section -->
-    <section>
-        <div class="container">
+ 		
+    <!-- For the main Section -->
+    <section style="background:linear-gradient(rgba(76,76,76,1), rgba(255, 0, 0, 0.55)),url('./images/woekdev.jpg'); height:100px; background-size: fixed; background-position:50% 50%;background-attachment: scroll; padding-top:20px; padding-bottom:10px; color: white;">
+    <style type="text/css">
+        input[type=date]
+        {
+            outline: none;
+            border: none;
+        }
+    </style>
+        <div class="container mdl-grid" style="margin-top:30px;">
 
-            <p><h3 style="font-family: 'LatoWebLight';">Events</h3> <span>From</span></p>
+            <div class="mdl-cell mdl-cell--3-col" style="height:60px;">
+                <span style="font-family: 'LatoWebLight'; font-size: 30px;">Events</span>
+            </div>
             
+            <div class="ruler" style="height: 40px; background: white; border:1px solid white; "> </div>
+        </div>
+    </section>
+
+    <section> <!-- Dynamic Calendar -->
+        <div class="container"  style="background: gray; height: 300px; padding-top: 40px;">
+           <center> <h2 style="margin-top:50px; padding-top: : 50px; text-align: center; font-family: 'LatoWebLight';">{{Dynamic Calendar!}}</h2></center>
         </div>
     </section>
 
